df721007fa5f5890707771e99aa0136f1cdd53f3
yamyamyuo <Nicole2014fall@163.com>
2018-02-12 18:14:29 +0800

    Update docker_compose_celery_entrypoint.sh

--- docker-compose/docker_compose_celery_entrypoint.sh
+++ docker-compose/docker_compose_celery_entrypoint.sh
@@ -6,18 +6,17 @@ cd $current_dir/../
 
 echo "init environment variables for dev & unit test..."
 export DOCKER_COMPOSE=1
-export SQL_BUFFET_UNIT_TEST=1
 export S3_PORT=4572
 export S3_HOST=localstack
 
-echo "start sqlbuffet worker..."
+echo "start worker..."
 export C_FORCE_ROOT="true"
 MYSQL_HOST='mysql'
 bash sbin/wait-for.sh $MYSQL_HOST:3306 -t 120
 bash sbin/wait-for.sh $S3_HOST:$S3_PORT -t 60
 
 echo "init localstack aws s3 bucket if not exists..."
-aws s3api --endpoint http://$S3_HOST:4572  create-bucket --bucket sqlbuffet
+aws s3api --endpoint http://$S3_HOST:4572  create-bucket --bucket s3_bucket
 aws s3 --endpoint http://$S3_HOST:4572 ls
 
 celery -A app.query.query_worker worker --loglevel=INFO
